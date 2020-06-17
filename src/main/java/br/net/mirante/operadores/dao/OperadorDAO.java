fd06e5f3ea6c3bb526e7677406ad294512299fb1
Lucas Couto <loc.unb@gmail.com>
2020-06-17 20:58:18 -0300

    criado outros métodos para a API Rest

--- src/main/java/br/net/mirante/operadores/dao/OperadorDAO.java
+++ src/main/java/br/net/mirante/operadores/dao/OperadorDAO.java
@@ -6,6 +6,8 @@ import br.net.mirante.operadores.entity.Operador;
 
 public interface OperadorDAO {
 	
-	public List<Operador> listarOperadores();
-
+	public List<Operador> listarOperadores();	
+	public Operador buscarOperador(int id);
+	public void cadastrarOperador(Operador operador);
+	public void deletarOperador(int id);
 }
--- src/main/java/br/net/mirante/operadores/dao/OperadorDAOHibernateImpl.java
+++ src/main/java/br/net/mirante/operadores/dao/OperadorDAOHibernateImpl.java
@@ -6,7 +6,6 @@ import javax.persistence.EntityManager;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import org.hibernate.*;
 
@@ -23,14 +22,34 @@ public class OperadorDAOHibernateImpl implements OperadorDAO {
 	}
 
 	@Override
-	@Transactional
 	public List<Operador> listarOperadores() {
 		
-		Session sessaoAtual = entityManager.unwrap(Session.class);
-		
-		List<Operador> operadores = sessaoAtual.createQuery("from Operador", Operador.class).getResultList();
-		
+		Session sessaoAtual = entityManager.unwrap(Session.class);		
+		List<Operador> operadores = sessaoAtual.createQuery("from Operador", Operador.class).getResultList();		
 		return operadores;
+	}
+
+	@Override
+	public Operador buscarOperador(int id) {
+		
+		Session sessaoAtual = entityManager.unwrap(Session.class);	
+		Operador operador = sessaoAtual.get(Operador.class, id);		
+		return operador;
+	}
+
+	@Override
+	public void cadastrarOperador(Operador operador) {
+		Session sessaoAtual = entityManager.unwrap(Session.class);
+		sessaoAtual.saveOrUpdate(operador);
+		
+	}
+
+	@Override
+	public void deletarOperador(int id) {		
+		Session sessaoAtual = entityManager.unwrap(Session.class);
+		sessaoAtual.createQuery("delete from Operador where id =:operadorId")
+			.setParameter("operadorId", id)
+			.executeUpdate();
 	}
 
 	
