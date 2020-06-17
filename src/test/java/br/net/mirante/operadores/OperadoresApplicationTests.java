5913d6b36803682ce0f6999f3272bd3fa413136b
Lucas Couto <loc.unb@gmail.com>
2020-06-16 22:09:53 -0300

    configuração inicial da API REST usando o Spring Boot

---
+++ src/test/java/br/net/mirante/operadores/OperadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.net.mirante.operadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
